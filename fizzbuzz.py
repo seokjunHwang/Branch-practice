8af40ed736eb9afaac6a192c8b9d2f758097706c
seokjunHwang <ada0004@naver.com>
2023-02-03 14:22:51 +0900

    feat : Add if Statement to print fizz
    Condition : i is multiple of 3.
    Otherwise, print i itself.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 # Get integer from user
 user_num = int(input('Hello, there! Enter some positive integer: '))
 
-#Print user_num for debugging
-print(user_num)
-
 #Iteration
 for i in range(1, user_num+1):
-    print(f'{i}')
+    if i%3==0:
+        print('fizz')
+    else:
+        print(f'{i}')
