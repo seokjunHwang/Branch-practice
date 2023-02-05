431257b3f7fc9b37a3bc0b565bebb8793d5898ce
seokjunHwang <ada0004@naver.com>
2023-02-05 23:27:17 +0900

    feat : Modified test1.py on test branch

--- test1.py
+++ test1.py
@@ -4,22 +4,4 @@
 
 import random as rd
 
-def meal(category, lists):
-    while True:
-      x = input("메뉴입력(종료는 'q'입력):")
-      if x == "q":
-        break
-      lists.append(x)
-      
-      inform = input(f"현재 {category}는 : {lists}입니다. \n더 고르시겠어요?( y or n 입력)")
-      print(inform)
-      while inform not in ["y", "n"]:
-        print("y , n 중 입력해주세요")
-        inform = input(f"현재 {category}는 : {lists}입니다. \n더 고르시겠어요?( y or n 입력)")
-      if inform == "n":
-        break
-
-menu_list = []
-meal("메뉴",menu_list)
-menu_recommendation = rd.choice(menu_list)
-print(f"추천메뉴는 {menu_recommendation}입니다.")
+print("1234")
